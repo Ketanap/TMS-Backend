d5b2aa6f35e7ec75eeecdea998461b06e9fc588d
KetanLogInt <ketan.logint@gmail.com>
2023-03-18 16:39:54 +0530

    Env and Cors Config

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require("express");
+var cors = require("cors");
+var env = require("dotenv");
 var routes = require("./routes/index.js");
 
 var app = express();
+app.use(cors());
 app.use(express.json());
-
 routes.init(app);
+env.config();
+console.log(process.env.MYSQL);
 app.get("/", function (req, res) {
   res.send("Api Launched");
 });
