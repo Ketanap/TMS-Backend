a445b6556bff7fde41f1ec5971c23721b051e2b8
Divyesh Parmar <divyesh.parmar@macymind.in>
2023-05-11 14:38:51 +0530

    11/05 change

--- routes/task-route.js
+++ routes/task-route.js
@@ -23,6 +23,27 @@ router.get("/", async function (req, res) {
     return res.status(401).send(error);
   }
 });
+router.get("/user/:userId", async function (req, res) {
+  console.log(req.header("authorization"));
+  try {
+    if (
+      jwt.verify(
+        req.header("authorization").substring(7),
+        process.env.JWT_SECRET_KEY
+      )
+    ) {
+      console.log(jwt.decode(req.header("authorization").substring(7)));
+      res.send(JSON.stringify(await task.getAllByUserId(req.params.userId)));
+    } else {
+      // Access Denied
+      return res.status(401).send(error);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send(error);
+  }
+});
+
 router.get("/:id", async function (req, res) {
   console.log(req.header("authorization"));
   try {
